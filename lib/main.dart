335e21116b144c97ba4b8224f4abafbe94f75a11
Segundo Bhumia <segundo@Segundos-iMac.frontierlocal.net>
2019-03-30 18:06:09 -0400

    initial commit

---
+++ lib/main.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+import './product_manager.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      theme: ThemeData(
+          primarySwatch: Colors.deepOrange,
+          accentColor: Colors.deepPurple,
+          brightness: Brightness.light),
+      home: Scaffold(
+          appBar: AppBar(
+              title: Center(
+            child: Text("EasyList"),
+          )),
+          body: ProductManager(
+            startingProduct: 'Food Tester',
+          )),
+    );
+  }
+}
---
+++ lib/product_manager.dart
@@ -0,0 +1,46 @@
+import 'package:flutter/material.dart';
+import './products.dart';
+import './product_control.dart';
+
+class ProductManager extends StatefulWidget {
+  final String startingProduct;
+
+  ProductManager({this.startingProduct = 'Sweets Tester'}) {
+    print('Products Manager constructor');
+  }
+
+  @override
+  State<StatefulWidget> createState() {
+    print('Products Manager createState()');
+    return _ProductManagerState();
+  }
+}
+
+class _ProductManagerState extends State<ProductManager> {
+  List<String> _products = [];
+
+  @override
+  void initState() {
+    super.initState();
+    print('Products Manager initState()');
+    _products.add(widget.startingProduct);
+  }
+
+  void _addProduct(String newProduct) {
+    this.setState(() {
+      _products.add(newProduct);
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(children: [
+      Container(
+          margin: EdgeInsets.all(10.0),
+          child: ProductControl(
+            addProduct: _addProduct,
+          )),
+      Products(this._products)
+    ]);
+  }
+}
